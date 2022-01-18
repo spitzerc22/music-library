7145ac997ad54d452ddfa2d65b0ff69bd8221cc2
Courtney Spitzer <spitzerc22@gmail.com>
2022-01-17 19:05:27 -0600

    implemented ref, dont like it, but okay

--- music-library/src/App.js
+++ music-library/src/App.js
@@ -1,42 +1,41 @@
 import Gallery from './Gallery';
 import Searchbar from './Searchbar';
-import {useState, useEffect} from 'react'
+import {useState, useEffect, useRef} from 'react'
 import { DataContext } from './context/DataContext';
+import { SearchContext } from './context/SearchContext';
 import './App.css'
 
 
 
 const App = () => {
-  let [search, setSearch] = useState('')
   let [message, setMessage] = useState('Search for Music!')
   let [data, setData] = useState([])
+  let searchInput = useRef('')
 
   const API_URL = 'https://itunes.apple.com/search?term='
 
-  useEffect(() => {
-      if(search) {
-          const fetchData = async () => {
-              document.title = `${search} Music`
-              const response = await fetch(API_URL + search)
-              const resData = await response.json()
-              if (resData.results.length > 0) {
-                  return setData(resData.results)
-              } else {
-                  return setMessage('Not Found.')
-              }
-          }
-        fetchData()
-      }
-  }, [search])
-
   const handleSearch = (e, term) => {
-      e.preventDefault()
-      setSearch(term)
-  }
+    e.preventDefault()
+    const fetchData = async () => {
+        const response = await fetch(API_URL + term)
+        const resData = await response.json()
+        if (resData.results.length > 0) {
+            return setData(resData.results)
+        } else {
+            return setMessage('Not Found.')
+        }
+    }
+    fetchData()
+}
 
   return (
       <div>
-          <Searchbar handleSearch={handleSearch} />
+        <SearchContext.Provider value={{
+          term: searchInput,
+          handleSearch: handleSearch
+        }}>
+            <Searchbar />
+        </SearchContext.Provider>
           {message}
           <DataContext.Provider value={data}>
             <Gallery />
